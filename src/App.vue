dd41aa87e624171188bb0c6e97d554dfcf41bcce
vklivak <vklivak@air.io>
2019-03-18 19:59:54 +0200

    Added basic calculator structure

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,27 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
-  </div>
+    <div id="app">
+        <header>
+            <h1>Calculator</h1>
+        </header>
+        <main>
+            <calculator></calculator>
+        </main>
+        <footer>
+
+        </footer>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+    import Calculator from './components/Calculator'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+    export default {
+        components: {
+            Calculator
+        }
+    }
 </script>
 
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+    @import "assets/scss/main.scss";
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="calculator">
+        <div class="calculator-container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+
+</style>
