9130943ef4139ab96162fc4bb5cc665ad2c8be33
vklivak <vladklivak@gmail.com>
2019-03-22 12:30:28 +0200

    Добавлено немного стилей

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -21,60 +21,60 @@
 
             <div class="row row-buttons">
                 <div class="col-9">
-                    <button @click="clear" class="btn btn-block btn-secondary">C</button>
+                    <button @click="clear" class="btn btn-block btn-secondary btn-symbol">C</button>
                 </div>
                 <div class="col-3">
-                    <button @click="math('+')" class="btn btn-block btn-secondary">+</button>
-                </div>
-
-                <div class="col-3">
-                    <button @click="number(7)" class="btn btn-block btn-primary">7</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(8)" class="btn btn-block btn-primary">8</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(9)" class="btn btn-block btn-primary">9</button>
+                    <button @click="math('+')" class="btn btn-block btn-secondary btn-symbol">+</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="math('-')" class="btn btn-block btn-secondary">-</button>
+                    <button @click="number(7)" class="btn btn-block btn-primary btn-number">7</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(8)" class="btn btn-block btn-primary btn-number">8</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(9)" class="btn btn-block btn-primary btn-number">9</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="number(4)" class="btn btn-block btn-primary">4</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(5)" class="btn btn-block btn-primary">5</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(6)" class="btn btn-block btn-primary">6</button>
+                    <button @click="math('-')" class="btn btn-block btn-secondary btn-symbol">-</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="math('*')" class="btn btn-block btn-secondary">*</button>
+                    <button @click="number(4)" class="btn btn-block btn-primary btn-number">4</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(5)" class="btn btn-block btn-primary btn-number">5</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(6)" class="btn btn-block btn-primary btn-number">6</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="number(1)" class="btn btn-block btn-primary">1</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(2)" class="btn btn-block btn-primary">2</button>
-                </div>
-                <div class="col-3">
-                    <button @click="number(3)" class="btn btn-block btn-primary">3</button>
+                    <button @click="math('*')" class="btn btn-block btn-secondary btn-symbol">*</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="math('/')" class="btn btn-block btn-secondary">/</button>
+                    <button @click="number(1)" class="btn btn-block btn-primary btn-number">1</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(2)" class="btn btn-block btn-primary btn-number">2</button>
+                </div>
+                <div class="col-3">
+                    <button @click="number(3)" class="btn btn-block btn-primary btn-number">3</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="number(0)" class="btn btn-block btn-primary">0</button>
+                    <button @click="math('/')" class="btn btn-block btn-secondary btn-symbol">/</button>
                 </div>
 
                 <div class="col-3">
-                    <button @click="math('.')" class="btn btn-block btn-primary">.</button>
+                    <button @click="number(0)" class="btn btn-block btn-primary btn-number">0</button>
+                </div>
+
+                <div class="col-3">
+                    <button @click="math('.')" class="btn btn-block btn-primary btn-symbol">.</button>
                 </div>
 
                 <div class="col-6">
